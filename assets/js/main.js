2c01ab9cdd50cd07df4898ff476f74a2a7bb1cca
Elga <elglalbarracin@misena.edu.co>
2022-08-08 17:25:17 -0500

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ document
   .getElementById("btn__iniciar-sesion")
   .addEventListener("click", iniciarSesion);
 document.getElementById("btn__registrarse").addEventListener("click", register);
+window.addEventListener("resizer", pageWidth);
 
 //VARIABLES
 var container_login_register = document.querySelector(
@@ -11,6 +12,22 @@ var form_login = document.querySelector(".form__login");
 var form_register = document.querySelector(".form__register");
 var back_box_login = document.querySelector(".back__box-login ");
 var back_box_register = document.querySelector(".back__box-register");
+
+function pageWidth() {
+  if (window.innerWidth > 850) {
+    back_box_login.style.display = "block";
+    back_box_register.style.display = "block";
+  } else {
+    back_box_register.style.display = "block";
+    back_box_register.style.opacity = "1";
+    back_box_login.style.display = "none";
+    form_login.style.display = "block";
+    form_register.style.display = "none";
+    container_login.register.style = "0px";
+  }
+}
+
+pageWidth();
 
 function iniciarSesion() {
   if (window.innerWidth > 850) {
